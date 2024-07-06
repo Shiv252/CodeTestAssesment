1395658ef647d30e2acd5c186fcadc3650d4349a
Shiv252 <58948329+Shiv252@users.noreply.github.com>
2024-07-06 23:05:24 +0000

    commit

--- FindLargeWord.java
+++ FindLargeWord.java
@@ -1,8 +1,9 @@
 public class FindLargeWord {
     
+
     public static void main(String[] args) {
         String sentence = "Hey, this is my accenture interview";
-        System.out.println("Longest word in the sentence is: " + findLongestWord(sentence));
+        System.out.println("Longest word in the sentence is : " + findLongestWord(sentence));
     }
     
     public static String findLongestWord(String sentence) {
